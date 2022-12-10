e3d39313389b96d88282106b5438672e42ef0197
Jiacheng Hu <76442535+UnicoCN@users.noreply.github.com>
2022-12-10 15:30:36 +0800

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,14 +1,3 @@
-ip_=$(cat /etc/resolv.conf)
-len=${#ip_}
-len=$((len-1))
-dns=""
-for ((i=$len;i>=0;i--))
-do
-	if [[ ${ip_:$i:1} != " " ]]; then
-		dns=${dns}${ip_:$i:1}
-	else
-		break
-	fi
-done
-dns=`echo $dns | rev`
-git config --global https.proxy ${dns}:7890
+#!/bin/bash
+host_ip=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")
+export ALL_PROXY="http://$host_ip:7890"
